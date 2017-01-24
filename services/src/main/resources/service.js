8f5d439cfa478a5c81e973e560707a0aa86f2d3e
Paulo Lopes <paulo@mlopes.net>
2017-01-24 11:56:05 +0100

    avoid exposing the inner details of the js facade

--- services/src/main/resources/service.js
+++ services/src/main/resources/service.js
@@ -3,10 +3,16 @@ var MyService = require('jetdrone-services-js/my_service');
 // force nashorn to convert to this type later on
 var JMyService = Java.type('io.jetdrone.services.MyService');
 
-MyService.registerService(vertx, 'io.jetdrone.services', {
-  _jdel: new JMyService ({
-    sayHello: function (handler) {
-      handler.handle(Future.succeededFuture('Hello there!'));
-    }
-  })
-});
+MyService.registerService(
+  vertx,
+  'io.jetdrone.services',
+  // this is the JS facade
+  new MyService(
+    // this is the Java Interface implementation
+    new JMyService({
+      sayHello: function (handler) {
+        handler.handle(Future.succeededFuture('Hello there!'));
+      }
+    })
+  )
+);
